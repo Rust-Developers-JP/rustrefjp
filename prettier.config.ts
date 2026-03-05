6248d4be8f0bc072d95c78b4e95eaf2425512837
ROBOT <tkata12@outlook.jp>
2026-03-05 21:37:02 +0900

    add: prettier

---
+++ prettier.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "prettier";
+
+const config: Config = {
+  semi: true,
+  singleQuote: false,
+  trailingComma: "all",
+  printWidth: 120,
+  useTabs: false,
+  tabWidth: 2,
+};
+
+export default config;
